25ab70d190be4b2bc930485badcf154078fd2a00
Darshan Patel <darsh1538@gmail.com>
2017-12-27 19:07:14 +0530

    starting with machine Learning

--- thatFood/ViewController.swift
+++ thatFood/ViewController.swift
@@ -7,6 +7,8 @@
 //
 
 import UIKit
+import CoreML
+import Vision
 
 class ViewController: UIViewController {
 
@@ -17,11 +19,7 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view, typically from a nib.
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
+    
 
 }
 
