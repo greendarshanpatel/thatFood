090438729c4786d24af3b9aabf37526eb293c5d6
Darshan Patel <darsh1538@gmail.com>
2017-12-27 19:31:58 +0530

    camera pressed action done.

--- thatFood/ViewController.swift
+++ thatFood/ViewController.swift
@@ -13,13 +13,18 @@ import Vision
 class ViewController: UIViewController,UIImagePickerControllerDelegate,UINavigationControllerDelegate {
 
     @IBOutlet weak var imageView: UIImageView!
+    let imagePicker = UIImagePickerController()
     @IBOutlet weak var label: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        imagePicker.delegate = self
+        imagePicker.sourceType = .camera
+        imagePicker.allowsEditing = false
     }
 
     @IBAction func cameraPressed(_ sender: UIBarButtonItem) {
+        present(imagePicker, animated: true, completion: nil)
     }
     
 
