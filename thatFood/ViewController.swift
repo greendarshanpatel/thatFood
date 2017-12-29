5376cf994eebda09c10b0c5715e139e5923e1fff
Darshan Patel <darsh1538@gmail.com>
2017-12-29 17:54:21 +0530

    getting predictions

--- thatFood/ViewController.swift
+++ thatFood/ViewController.swift
@@ -19,18 +19,28 @@ class ViewController: UIViewController,UIImagePickerControllerDelegate,UINavigat
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         imagePicker.delegate = self
-        imagePicker.sourceType = .camera
+        imagePicker.sourceType = .photoLibrary
         imagePicker.allowsEditing = false
     }
 
     func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]) {
         if let userPickedImage = info[UIImagePickerControllerOriginalImage] as? UIImage{
             imageView.image = userPickedImage
+            guard let ciimage = CIimage(image:userPickedImage) else {
+                fatalErro["couldn't convert uiimage in ciimage"]
+            }
+            detect(image: ciimage)
         }
         imagePicker.dismiss(animated: true, completion: nil)
     }
+    
+    
     @IBAction func cameraPressed(_ sender: UIBarButtonItem) {
         present(imagePicker, animated: true, completion: nil)
+    }
+    
+    func detect(umage: CIImage){
+        
     }
     
 
